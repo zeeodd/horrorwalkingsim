d1beafebea08d5e1552aad0fb37792766fb479b9
zeeodd <zad22@cornell.edu>
2019-10-06 18:53:43 -0400

    initial push

---
+++ HorrorWalkingSim/Assets/First Person Drifter Controller/Scripts/LockMouse.cs
@@ -0,0 +1,41 @@
+﻿// by @torahhorse
+
+using UnityEngine;
+using System.Collections;
+
+public class LockMouse : MonoBehaviour
+{
+    public Texture2D cursorTexture;
+
+	void Start()
+	{
+		LockCursor(true);
+
+        Cursor.SetCursor(cursorTexture,
+                         new Vector2(cursorTexture.width / 2, cursorTexture.height / 2),
+                         CursorMode.Auto);
+        Cursor.visible = true;
+	}
+
+    void Update()
+    {
+    	// lock when mouse is clicked
+    	if( Input.GetMouseButtonDown(0) && Time.timeScale > 0.0f )
+    	{
+            LockCursor(true);
+    	}
+
+        //Cursor.visible = true;
+    
+    	// unlock when escape is hit
+        if  ( Input.GetKeyDown(KeyCode.Escape) )
+        {
+        	LockCursor(!Screen.lockCursor);
+        }
+    }
+    
+    public void LockCursor(bool lockCursor)
+    {
+    	Screen.lockCursor = lockCursor;
+    }
+}
