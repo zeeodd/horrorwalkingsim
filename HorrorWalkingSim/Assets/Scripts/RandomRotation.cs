619a786eb0e7d856a25dc79416b55550e3faebbd
zeeodd <zad22@cornell.edu>
2019-10-09 20:28:41 -0400

    Setting Up Cornfield
    1. Made FPC run a little faster 2. Set up new corn model 3. Started building the corn maze

---
+++ HorrorWalkingSim/Assets/Scripts/RandomRotation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotation : MonoBehaviour
+{
+    private Quaternion q;
+
+    void Start()
+    {
+        Vector3 v;
+
+        v.x = -90.0f;
+        v.y = 0.0f; ;
+        v.z = Random.Range(0.0f, 360.0f);
+
+        q = Quaternion.Euler(v);
+
+        transform.rotation = q;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
