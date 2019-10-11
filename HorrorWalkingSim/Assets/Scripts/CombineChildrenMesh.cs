7235f5464f1560af22ed7efea388b5bd39379d0c
zeeodd <zad22@cornell.edu>
2019-10-11 13:34:52 -0400

    Init Push on This Branch
    1. First draft of combinemesh 2. Cornfield set up with slowdown script 3. Corn model prefab modified

---
+++ HorrorWalkingSim/Assets/Scripts/CombineChildrenMesh.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+
+public class CombineChildrenMesh : MonoBehaviour
+{
+    void Start()
+    {
+        combineMeshes();
+    }
+
+    void combineMeshes()
+    {
+        MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
+        CombineInstance[] combine = new CombineInstance[meshFilters.Length];
+
+        int i = 0;
+        while (i < meshFilters.Length)
+        {
+            combine[i].mesh = meshFilters[i].sharedMesh;
+            combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
+            meshFilters[i].gameObject.SetActive(false);
+
+            i++;
+        }
+
+        var meshFilter = transform.GetComponent<MeshFilter>();
+        meshFilter.mesh = new Mesh();
+        meshFilter.mesh.CombineMeshes(combine);
+        GetComponent<MeshCollider>().sharedMesh = meshFilter.mesh;
+        transform.gameObject.SetActive(true);
+
+        //transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        //transform.rotation = Quaternion.identity;
+        //transform.position = Vector3.zero;
+    }
+}
---
+++ HorrorWalkingSim/Assets/Scripts/CornSlowDown.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CornSlowDown : MonoBehaviour
+{
+    private Collider lastCornTrigger;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        lastCornTrigger = other;
+
+        if (other.tag == "Corn")
+        {
+            GetComponent<FirstPersonDrifter>().walkSpeed = 3;
+            GetComponent<FirstPersonDrifter>().runSpeed = 5;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (lastCornTrigger == other)
+        {
+            GetComponent<FirstPersonDrifter>().walkSpeed = 6;
+            GetComponent<FirstPersonDrifter>().runSpeed = 10;
+        }
+    }
+}
