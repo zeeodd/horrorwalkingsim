494dca896506cf774886a654c12c1dd58aaa0c4e
zeeodd <zad22@cornell.edu>
2019-10-17 08:24:55 -0400

    New Maze Update
    1. Swapped out old maze for 2D corn maze 2. Updated player prefab 3. Added run input setting

---
+++ HorrorWalkingSim/Assets/Scripts/LookAtPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtPlayer : MonoBehaviour
+{
+    public GameObject player;
+
+    void Update()
+    {
+       transform.forward = player.transform.forward;
+    }
+}
