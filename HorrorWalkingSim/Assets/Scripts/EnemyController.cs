a903480f539a3979c1f5e228b0ad461dcb03a0d9
zeeodd <zad22@cornell.edu>
2019-10-21 10:26:59 -0400

    Implement Enemy
    1. Animated the enemy 2. Implemented a basic enemy controller script

---
+++ HorrorWalkingSim/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    // Speed control
+    private float speed;
+    private float w_speed = 6.0f;
+    private float r_speed = 12.0f;
+
+    // Grab model components
+    Rigidbody rb;
+    Animator anim;
+    CapsuleCollider col_size;
+
+    // Get Player
+    public GameObject player;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        col_size = GetComponent<CapsuleCollider>();
+
+        anim.SetBool("isIdle", true);
+        anim.SetBool("isWalking", false);
+        anim.SetBool("isRunning", false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.I))
+        {
+            speed = w_speed;
+
+            anim.SetBool("isIdle", false);
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isRunning", true);
+        }
+    }
+}
